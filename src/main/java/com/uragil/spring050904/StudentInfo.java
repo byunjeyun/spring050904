4b13cf1b75264acf390140428548474083b2651e
JBEDU29 <JBEDU29@DESKTOP-9PBMPIR>
2022-05-09 16:18:43 +0900

    applicationCTX nameSpcace

---
+++ src/main/java/com/uragil/spring050904/StudentInfo.java
@@ -0,0 +1,22 @@
+package com.uragil.spring050904;
+
+public class StudentInfo {
+
+	private Student student;
+
+	public StudentInfo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	
+	
+	
+}
